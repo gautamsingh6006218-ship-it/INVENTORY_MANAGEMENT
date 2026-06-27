62b2854720923683c7adcb252bb3dda3e7508b76
Gautam Singh <gautamsingh@Gautams-MacBook-Air.local>
2026-06-27 18:57:57 +0530

    modified settigs.py register account,db , postgres sql, JWT, auth model models.py used add user details and role serlializer.py convert json from reeact to python obj

--- backend/user-service/accounts/models.py
+++ backend/user-service/accounts/models.py
@@ -1,3 +1,40 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class User(AbstractUser):
+
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('staff', 'Staff'),
+    )
+
+    email = models.EmailField(unique=True)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='staff')
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def is_Admin(self):
+        return self.role == 'admin'
+
+    def is_Staff_member(self):
+        return self.role == 'staff'
+    
+    def get_full_details(self):
+        return {
+            'id': self.id,
+            'email': self.email,
+            'username': self.username,
+            'role': self.role,
+            'phone': self.phone,
+        }
+    
+    def __str__(self):
+        return f"{self.email} ({self.role})"
+    
+    
--- backend/user-service/user_service/settings.py
+++ backend/user-service/user_service/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -74,8 +77,12 @@ WSGI_APPLICATION = "user_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "user_service_db",
+        "USER": "inventory_user",
+        "PASSWORD": "inventory123",
+        "HOST": "localhost",
+        "PORT": "5432"
     }
 }
 
@@ -115,3 +122,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+from datetime import timedelta
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+}
+
+AUTH_USER_MODEL = 'accounts.User'
